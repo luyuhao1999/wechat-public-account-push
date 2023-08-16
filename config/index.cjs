e60d29462116042b59cf3fa2e2c043e9b84ef3be
luyuhao1999 <105032594+luyuhao1999@users.noreply.github.com>
2023-08-16 15:39:46 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -129,22 +129,9 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '小嘉', year: '1998', date: '05-05',
+          type: '*生日', name: '小嘉', year: '1998', date: '05-05',isShowAge: true
         }
-      ],
-      // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
-      customizedDateList: [
-        // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
-        // 退伍日
-        { keyword: 'ex_day', date: '2022-09-10' }
-      ],
-      // 课程表相关配置
-      // 如果courseSchedule不存在或者为空（null）则认为没有课程
-      // 如果courseSchedule是一个数组，则认为不区分单双周，直接填写星期几对应的课表数据即可
-      // 如果courseSchedule是一个对象（如下面所示）
+      ]
     }
     // 你可以不断按格式往下增加
     // ...
