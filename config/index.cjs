7e8b818ac4b17d2eba3dadb56343c302ac2191c1
luyuhao1999 <105032594+luyuhao1999@users.noreply.github.com>
2023-08-16 15:25:47 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -73,7 +73,7 @@ const USER_CONFIG = {
   
   TIAN_API: {
     // 天行API KEY，如果使用天行API则需要填写此项
-    key: '',
+    key: '894b9e92ab9eda987827524de11311c2',
     
     /** 天行API相关，需要config中配置 TIAN_API_KEY  */
     // 早安心语, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
