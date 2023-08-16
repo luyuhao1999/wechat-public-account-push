f67458d215f5412eb7482e8db789ee466f133a12
luyuhao1999 <105032594+luyuhao1999@users.noreply.github.com>
2023-08-16 15:20:14 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -111,7 +111,7 @@ const USER_CONFIG = {
       name: '小嘉',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       // 使用其他通道时，请严格按照各个通道的教程进行填写
-      id: '',
+      id: 'oKkJH6qtEXINBR0vfV6CP2dEpMb0',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       // 使用其他通道时，请填写 config/template-config.cjs 中某个想要使用的模板的id
       useTemplateId: 'I1O1Eg4MKQO9auBRTeZTYoeoCLplAogD0gg3gklE_N4',
