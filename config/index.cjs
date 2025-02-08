198c3154a339e0a183515d79cd98420c2da07198
luyuhao1999 <105032594+luyuhao1999@users.noreply.github.com>
2025-02-08 09:20:12 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -45,11 +45,34 @@ const USER_CONFIG = {
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '6cSbkzdo6tqzBMqACDGZht6gWES2U1bO25BgB_uZ0S0',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '05-05',
+      horoscopeDate: '08-12',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
           type: '*生日', name: '琳琳宝宝', year: '2001', date: '08-12',
+        }
+      ],
+      // 我们在一起已经有xxxx天了的配置
+      customizedDateList: [
+        // 在一起的日子
+        { keyword: 'love_day', date: '2023-06-24' },
+        // 结婚纪念日
+        { keyword: 'marry_day', date: '2022-09-09' },
+      ],
+    },
+    {
+      // 想要发送的人的名字
+      name: '卢禹豪',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'opnDo6_ikFd-2Q4u-ALhvaBUuhRE',
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      useTemplateId: '6cSbkzdo6tqzBMqACDGZht6gWES2U1bO25BgB_uZ0S0',
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscopeDate: '11-09',
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {
+          type: '*生日', name: '卢禹豪', year: '1999', date: '11-09',
         }
       ],
       // 我们在一起已经有xxxx天了的配置
