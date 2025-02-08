f717e48030b62afdbb13baf3aee0dc435c39f780
luyuhao1999 <105032594+luyuhao1999@users.noreply.github.com>
2025-02-08 09:21:52 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -41,7 +41,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '琳琳宝宝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'opnDo6_ikFd-2Q4u-ALhvaBUuhRE',
+      id: 'opnDo69vfhQErb29eGiueTXdE27g',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '6cSbkzdo6tqzBMqACDGZht6gWES2U1bO25BgB_uZ0S0',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
